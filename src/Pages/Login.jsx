188857104a3fcd9f29a746eb212661462bb3dce0
Genni02 <jenniferpalacin02@gmail.com>
2025-04-29 00:46:03 -0500

    Ramita de conflictos

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -17,8 +17,32 @@ const Login = () => {
       <div className="bg-white p-8 rounded-2xl shadow-md w-80">
         <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
         <form onSubmit={handleSubmit}>
-         
-          
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2" htmlFor="user">
+              Usuario
+            </label>
+            <input
+              type="text"
+              id="user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              className="w-full p-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm font-semibold mb-2" htmlFor="password">
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500"
+              required
+            />
+          </div>
           <button
             type="submit"
             className="w-full bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-xl transition duration-300"
